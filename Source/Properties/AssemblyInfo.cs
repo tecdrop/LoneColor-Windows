6706f553815d13a8482541ff6de11f85425004ec
appgramming <dev@appgramming.com>
2023-12-16 20:14:16 +0200

    Add installer
    + Improve app icon
    + Update app version

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.90")]
-[assembly: AssemblyFileVersion("3.0.0.90")]
+[assembly: AssemblyVersion("3.1.0.100")]
+[assembly: AssemblyFileVersion("3.1.0.100")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 [assembly: CLSCompliant(true)]
